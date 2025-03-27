2ccdb1780ea170108543711ec0e83d9bb1c561be
afifao1 <afifao1>
2025-03-28 03:09:23 +0500

    PATCH: Added login and register files

---
+++ login.php
@@ -0,0 +1,11 @@
+<form action="login.php">
+    <div>
+        <h1>Login page</h1>
+        <label for="email">Email</label>
+        <input type="email" placeholder="Enter a email" name="email" required><br><br>
+        <label for="password">Password</label>
+        <input type="password" placeholder="Enter a password" name="password" required><br><br>
+        <button type="submit">submit</button><br><br>
+        <a href="register.php">not registered before</a>
+    </div>
+</form>
